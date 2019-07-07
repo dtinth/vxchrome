1471e127ff068642bb8aece2798ff8fce1a1f346
iproz <nanthakarn18@gmail.com>
2019-07-07 14:20:22 +0700

    use for loop to select item(set value???) * storage.sync??? * problem unknow lang in select item(options.html)

--- options.js
+++ options.js
@@ -1,1 +1,97 @@
+var langs = [
+  ['Afrikaans', ['af-ZA']],
+  ['Bahasa Indonesia', ['id-ID']],
+  ['Bahasa Melayu', ['ms-MY']],
+  ['Català', ['ca-ES']],
+  ['Čeština', ['cs-CZ']],
+  ['Dansk', ['da-DK']],
+  ['Deutsch', ['de-DE']],
+  ['English', ['en-AU', 'Australia'],
+    ['en-CA', 'Canada'],
+    ['en-IN', 'India'],
+    ['en-NZ', 'New Zealand'],
+    ['en-ZA', 'South Africa'],
+    ['en-GB', 'United Kingdom'],
+    ['en-US', 'United States']
+  ],
+  ['Español', ['es-AR', 'Argentina'],
+    ['es-BO', 'Bolivia'],
+    ['es-CL', 'Chile'],
+    ['es-CO', 'Colombia'],
+    ['es-CR', 'Costa Rica'],
+    ['es-EC', 'Ecuador'],
+    ['es-SV', 'El Salvador'],
+    ['es-ES', 'España'],
+    ['es-US', 'Estados Unidos'],
+    ['es-GT', 'Guatemala'],
+    ['es-HN', 'Honduras'],
+    ['es-MX', 'México'],
+    ['es-NI', 'Nicaragua'],
+    ['es-PA', 'Panamá'],
+    ['es-PY', 'Paraguay'],
+    ['es-PE', 'Perú'],
+    ['es-PR', 'Puerto Rico'],
+    ['es-DO', 'República Dominicana'],
+    ['es-UY', 'Uruguay'],
+    ['es-VE', 'Venezuela']
+  ],
+  ['Euskara', ['eu-ES']],
+  ['Filipino', ['fil-PH']],
+  ['Français', ['fr-FR']],
+  ['Galego', ['gl-ES']],
+  ['Hrvatski', ['hr_HR']],
+  ['IsiZulu', ['zu-ZA']],
+  ['Íslenska', ['is-IS']],
+  ['Italiano', ['it-IT', 'Italia'],
+    ['it-CH', 'Svizzera']
+  ],
+  ['Lietuvių', ['lt-LT']],
+  ['Magyar', ['hu-HU']],
+  ['Nederlands', ['nl-NL']],
+  ['Norsk bokmål', ['nb-NO']],
+  ['Polski', ['pl-PL']],
+  ['Português', ['pt-BR', 'Brasil'],
+    ['pt-PT', 'Portugal']
+  ],
+  ['Română', ['ro-RO']],
+  ['Slovenščina', ['sl-SI']],
+  ['Slovenčina', ['sk-SK']],
+  ['Suomi', ['fi-FI']],
+  ['Svenska', ['sv-SE']],
+  ['Tiếng Việt', ['vi-VN']],
+  ['Türkçe', ['tr-TR']],
+  ['Ελληνικά', ['el-GR']],
+  ['български', ['bg-BG']],
+  ['Pусский', ['ru-RU']],
+  ['Српски', ['sr-RS']],
+  ['Українська', ['uk-UA']],
+  ['한국어', ['ko-KR']],
+  ['中文', ['cmn-Hans-CN', '普通话 (中国大陆)'],
+    ['cmn-Hans-HK', '普通话 (香港)'],
+    ['cmn-Hant-TW', '中文 (台灣)'],
+    ['yue-Hant-HK', '粵語 (香港)']
+  ],
+  ['日本語', ['ja-JP']],
+  ['हिन्दी', ['hi-IN']],
+  ['ภาษาไทย', ['th-TH']]
+];
+
+for (var i = 0; i < langs.length; i++) {
+  select_first_language.options[i] = new Option(langs[i][0], i);
+}
+for (var i = 0; i < langs.length; i++) {
+  select_second_language.options[i] = new Option(langs[i][0], i);
+}
+
+//function updateCountry() {
+//  for (var i = select_dialect.options.length - 1; i >= 0; i--) {
+//    select_dialect.remove(i);
+//  }
+//  var list = langs[select_language.selectedIndex];
+//  for (var i = 1; i < list.length; i++) {
+//    select_dialect.options.add(new Option(list[i][1], list[i][0]));
+//  }
+//  select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+//}
+
 // Saves options to chrome.storage
