ca0669ffd0447dc431ce069acf2d09d01ee43062
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-17 11:30:54 +0700

    Prettify code

--- background.js
+++ background.js
@@ -22,15 +22,15 @@ function listen(lang) {
 }
 
 chrome.commands.onCommand.addListener(function(command) {
-  chrome.storage.sync.get(["language1", "language2"], function(items) {
-      language1 = items.language1;
-      language2 = items.language2;
-      if (command === 'listen-en') {
-        listen(language1)
-      }
-      if (command === 'listen-th') {
-        listen(language2)
-      }
+  chrome.storage.sync.get(['language1', 'language2'], function(items) {
+    language1 = items.language1
+    language2 = items.language2
+    if (command === 'listen-en') {
+      listen(language1)
+    }
+    if (command === 'listen-th') {
+      listen(language2)
+    }
   })
 })
 
@@ -44,11 +44,8 @@ chrome.windows.onRemoved.addListener(function(windowId) {
 chrome.browserAction.onClicked.addListener(buttonClicked)
 function buttonClicked() {
   if (chrome.runtime.openOptionsPage) {
-    chrome.runtime.openOptionsPage();
+    chrome.runtime.openOptionsPage()
   } else {
-    window.open(chrome.runtime.getURL('options.html'));
+    window.open(chrome.runtime.getURL('options.html'))
   }
-};
-
-
-
+}
