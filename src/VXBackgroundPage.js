b0b6a67dd7ec1fcde6bdf92b39cd0872c030e681
Thai Pangsakulyanont <dtinth@spacet.me>
2020-04-04 04:52:00 +0700

    Remove extraneous code

--- src/VXBackgroundPage.js
+++ src/VXBackgroundPage.js
@@ -8,16 +8,7 @@ function listen(lang, settings) {
   vx.toggle({ lang, settings })
 }
 
-function injectContentScript() {
-  chrome.tabs.executeScript({ code: 'void 0' }, () => {
-    if (chrome.runtime.lastError) {
-      // console.log('T_T', chrome.runtime.lastError.message)
-    }
-  })
-}
-
 chrome.commands.onCommand.addListener(function(command) {
-  // injectContentScript()
   chrome.storage.sync.get(defaultSettings, function(settings) {
     if (command === 'listen1') {
       listen(settings.language1, settings)
@@ -29,7 +20,6 @@ chrome.commands.onCommand.addListener(function(command) {
 })
 
 chrome.browserAction.onClicked.addListener(function() {
-  // injectContentScript()
   chrome.storage.sync.get(defaultSettings, function(settings) {
     listen(settings.language1, settings)
   })
