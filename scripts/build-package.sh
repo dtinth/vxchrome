12a057090bc495862b85fbf540234ce7e976259d
Thai Pangsakulyanont <dtinth@spacet.me>
2020-04-04 04:51:48 +0700

    Name it "vx"

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -6,5 +6,6 @@ echo "$FILE"
 rm -rf tmp/package tmp/vx.zip
 mkdir -p tmp
 tar xvzf "$FILE" -C tmp
-(cd tmp/package && zip -r ../vx.zip .)
+mv tmp/package tmp/vx
+(cd tmp/vx && zip -r ../vx.zip .)
 rm "$FILE"
