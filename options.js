e9de482a018f9d7f9da6698384245e1a8902f467
Thai Pangsakulyanont <dtinth@spacet.me>
2020-04-03 23:29:41 +0700

    Move save button

--- options.js
+++ options.js
@@ -1,6 +1,32 @@
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
+{
+  const html = String.raw
+
+  Vue.component('save-button', {
+    props: ['store'],
+    template: html`
+      <div class="flex items-center mb-4">
+        <div class="w-1/3"></div>
+        <div>
+          <button
+            @click="store.save"
+            class="shadow text-090807 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded"
+            :class="[store.changed ? 'bg-d7fc70' : 'bg-8b8685']"
+            type="button"
+          >
+            Save
+          </button>
+        </div>
+        <div class="ml-4">
+          {{store.status}}
+        </div>
+      </div>
+    `,
+  })
+}
+
 var vm = new Vue({
   el: '#app',
   data: {
